f9047ad693fb42130854f14cfcbd740529f48ab9
hoang nam <namhvth2204008@fpt.edu.vn>
2022-09-20 10:26:40 +0700

    1

--- nodeJS/Newdemo/src/config/conDOAN.js
+++ nodeJS/Newdemo/src/config/conDOAN.js
@@ -1,21 +1,23 @@
-// import mysql from 'mysql2'
+import { connect } from 'mssql'
+import mysql from 'mysql2'
 
-// const cnnn = mysql.createConnection({
-//     host: "pokabi.tech",
-//     user: "admin_edit",
-//     password: "adminedit",
-//     database: "Eproject",
-//     // port: 3306,
-//     // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+const cnnn = mysql.createConnection({
+    host: "123.16.16.117", //  123.16.16.117  - api.pokabi.tech -  pokabi.tech
+    user: "admin_edit",
+    password: "adminedit",
+    database: "Eproject",
+    // port: 3306,
+    // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+})
+
+cnnn.connect((err) => {
+    if (err) { console.log(err) } else {
+        console.log('connected conDO-AN')
+    }
+})
+cnnn.query('select * from Register')
+
+// cnnn.query('SELECT * FROM `category` WHERE 1', (err, data) => {
+//     console.log(data)
 // })
-
-// cnnn.connect((err) => {
-//     if (err) { console.log(err) } else {
-//         console.log('connected conDO-AN')
-//     }
-// })
-
-// // cnnn.query('SELECT * FROM `category` WHERE 1', (err, data) => {
-// //     console.log(data)
-// // })
-// export default cnnn;
+export default cnnn;
